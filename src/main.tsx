f1879ce80b77975e2393735b5d85bef98a1a478a
Jesus Alcala <jesusalcalarojas@gmail.com>
2025-10-22 21:06:53 -0400

    feat: remove import font space-grotesk

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import "@fontsource-variable/space-grotesk";
+import { App } from "./app";
 import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
