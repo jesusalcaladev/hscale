a7ddcd0792f8999483f49aa46dc474e1d200c427
Jesus Alcala <jesusalcalarojas@gmail.com>
2025-10-22 21:08:25 -0400

    feat: create hook use-palette

---
+++ src/hooks/use-palette.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { generatePalette } from "../utils/generate-palatte";
+import type { Shade } from "../types/shade";
+
+const DEFAULT_SHADES_COUNT = 9;
+
+export const usePalette = () => {
+  const [shadesCount, setShadesCount] = useState<number>(DEFAULT_SHADES_COUNT);
+  const [color, setColor] = useState<string>("#121212");
+  const [name, setName] = useState<string>("custom");
+  const [shades, setShades] = useState<Shade[]>(
+    generatePalette(color, shadesCount),
+  );
+
+  useEffect(() => {
+    const newShades = generatePalette(color, shadesCount);
+    setShades(newShades);
+  }, [color, shadesCount]);
+
+  const settingProps = {
+    color,
+    setColor,
+    name,
+    setName,
+    shadesCount,
+    setShadesCount,
+    shades,
+  };
+
+  return { shades, name, color, settingProps };
+};
